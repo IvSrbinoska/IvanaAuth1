4d1d0de85754f0dad940abb770ad6a7fe54ff250
mk2010 <vladislav.skoro@gmail.com>
2018-04-28 14:11:48 -0400

    Add project files.

---
+++ IvanaAuth1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IvanaAuth1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
